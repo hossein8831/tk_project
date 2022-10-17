a35f4ec27a0ed2c8e26a3dae8433cba73f1f1370
hossein8831 <115214704+hossein8831@users.noreply.github.com>
2022-10-17 03:10:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,41 @@
 from tkinter import *
 import wikipedia as wk
 import speech_recognition as sr
+import random
+from PIL import ImageTk,Image
+import urllib.request
 wk.set_lang('fa')
 win = Tk()
 win.geometry('700x550')
 win.iconbitmap('wiki.ico')
 win.title('wiki search with Tk')
 def takeCommand():
-    global query
     r = sr.Recognizer()
     with sr.Microphone() as source:
-            r.pause_threshold = 1
+            r.pause_threshold = 3
             audio = r.listen(source)
-            msg_1.configure(text='yezare')
             try:
                 query = r.recognize_google(audio, language ='fa-IR', show_all=False)
                 label_1.configure(text=query)
-                result_wk = wk.summary(query,sentences=2)
+                result_wk = wk.summary(query,sentences=3)
                 msg_1.configure(text=result_wk)
-        
+                wk.set_lang('fa')
+                wer =wk.page(query).images
+                urllib.request.urlretrieve(wer[random.randrange(1,11)],'sd.jpg')
+                img = Image.open('sd.jpg')
+                img = img.resize((300,250),Image.ANTIALIAS)
+                img = ImageTk.PhotoImage(img)
+                pj.configure(image=img)
+                pj.image =img
             except Exception as e:
                 label_1.configure(text='متوجه نشدم')
+                msg_1.configure(text=':(')
 label_1 = Label(text='None',fg='blue',bg='yellow',font='bold')
 label_1.pack(ipadx='25')
 button_1 = Button(text='سرچ کن',command=takeCommand)
 button_1.pack(ipadx='30',ipady='8')
-msg_1 = Message(text='  ',fg='green',font='bold')
-
-label_2 = Label(text=query)
-label_2.pack()
-msg_1.pack()
+pj = Label(image= None)
+pj.pack()
+msg_1 = Message(text='.',font='bold',fg='green')
+msg_1.pack(pady='10')
 win.mainloop()
